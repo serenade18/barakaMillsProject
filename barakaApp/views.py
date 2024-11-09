65560149b65be70c3d83d3ade2136953d1238687
serenade18 <seretaahmed18@gmail.com>
2024-11-09 11:01:34 +0300

    Added user accounts viewsets

--- barakaApp/views.py
+++ barakaApp/views.py
@@ -31,7 +31,122 @@ class UserInfoView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class UserViewSet(viewsets.ViewSet):
+class AdminUserViewSet(viewsets.ViewSet):
+    permission_classes_by_action = {
+        'create': [AllowAny],
+        'verify_otp': [AllowAny],
+        'default': [IsAuthenticated]
+    }
+
+    def get_permissions(self):
+        return [permission() for permission in self.permission_classes_by_action.get(self.action, self.permission_classes_by_action['default'])]
+
+    def create(self, request):
+        # Remove 'car_reg' from the request data if present and explicitly set user_type to 'admin'
+        modified_data = request.data.copy()
+        modified_data.pop('car_reg', None)  # Remove 'car_reg' key if it exists
+        modified_data['user_type'] = 'admin'  # Explicitly set user type to admin
+
+        serializer = UserCreateSerializer(data=modified_data)
+        if serializer.is_valid():
+            # Hash the password before saving the user
+            password = make_password(serializer.validated_data['password'])
+            serializer.validated_data['password'] = password
+
+            # Generate OTP
+            otp = random.randint(100000, 999999)
+            email = serializer.validated_data['email']
+
+            # Send OTP to email
+            send_mail(
+                'Your OTP Code',
+                f'Your OTP code is {otp}',
+                'from@example.com',
+                [email],
+                fail_silently=False,
+            )
+
+            # Save OTP to the database
+            OTP.objects.create(email=email, otp=otp)
+
+            # Set the user as inactive
+            serializer.validated_data['is_active'] = False
+            serializer.save()
+
+            # Save OTP and email in session
+            request.session['otp'] = otp
+            request.session['email'] = email
+
+            return Response({'message': 'OTP sent to email'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def verify_otp(self, request):
+        otp = request.data.get('otp')
+
+        if not otp:
+            return Response({'error': 'Missing OTP'}, status=status.HTTP_400_BAD_REQUEST)
+
+        email = request.session.get('email')
+        session_otp = request.session.get('otp')
+
+        if otp != str(session_otp):
+            return Response({'error': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            otp_record = OTP.objects.get(email=email, otp=otp)
+        except OTP.DoesNotExist:
+            return Response({'error': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not otp_record.is_valid():
+            return Response({'error': 'OTP expired'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Activate the user and set them as admin
+        try:
+            user = UserAccount.objects.get(email=email)
+            user.is_superuser = True
+            user.is_active = True
+            user.is_staff = True
+            user.user_type = 'admin'
+            user.save()
+        except UserAccount.DoesNotExist:
+            return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Clear session data and delete OTP
+        del request.session['otp']
+        del request.session['email']
+        otp_record.delete()
+
+        return Response({'message': 'User activated successfully'}, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        queryset = UserAccount.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserAccountSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class HybridUserViewSet(viewsets.ViewSet):
     permission_classes_by_action = {'create': [AllowAny], 'list': [IsAdminUser], 'verify_otp': [AllowAny], 'default': [IsAuthenticated]}
 
     def get_permissions(self):
@@ -110,6 +225,7 @@ class UserViewSet(viewsets.ViewSet):
         try:
             user = UserAccount.objects.get(email=email)
             user.is_active = True
+            user.user_type = 'hybrid'
             user.save()
         except UserAccount.DoesNotExist:
             return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
@@ -149,23 +265,29 @@ class UserViewSet(viewsets.ViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class AdminUserViewSet(viewsets.ViewSet):
-    permission_classes_by_action = {
-        'create': [AllowAny],
-        'verify_otp': [AllowAny],
-        'default': [IsAuthenticated]
-    }
+class SalesUserViewSet(viewsets.ViewSet):
+    permission_classes_by_action = {'create': [AllowAny], 'list': [IsAdminUser], 'verify_otp': [AllowAny], 'default': [IsAuthenticated]}
 
     def get_permissions(self):
         return [permission() for permission in self.permission_classes_by_action.get(self.action, self.permission_classes_by_action['default'])]
 
-    def create(self, request):
-        # Remove 'car_reg' from the request data if present and explicitly set user_type to 'admin'
-        modified_data = request.data.copy()
-        modified_data.pop('car_reg', None)  # Remove 'car_reg' key if it exists
-        modified_data['user_type'] = 'admin'  # Explicitly set user type to admin
+    def list(self, request):
+        try:
+            users = UserAccount.objects.all()
+            serializer = UserAccountSerializer(users, many=True, context={"request": request})
+            response_data = serializer.data
+            response_dict = {"error": False, "message": "All Users List Data", "data": response_data}
 
-        serializer = UserCreateSerializer(data=modified_data)
+        except ValidationError as e:
+            response_dict = {"error": True, "message": "Validation Error", "details": str(e)}
+        except Exception as e:
+            response_dict = {"error": True, "message": "An Error Occurred", "details": str(e)}
+
+        return Response(response_dict,
+                        status=status.HTTP_400_BAD_REQUEST if response_dict['error'] else status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = UserCreateSerializer(data=request.data)
         if serializer.is_valid():
             # Hash the password before saving the user
             password = make_password(serializer.validated_data['password'])
@@ -218,13 +340,130 @@ class AdminUserViewSet(viewsets.ViewSet):
         if not otp_record.is_valid():
             return Response({'error': 'OTP expired'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Activate the user and set them as admin
+        # Activate the user
         try:
             user = UserAccount.objects.get(email=email)
-            user.is_superuser = True
             user.is_active = True
-            user.is_staff = True
-            user.user_type = 'admin'
+            user.user_type = 'sales'
+            user.save()
+        except UserAccount.DoesNotExist:
+            return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Clear session data and delete OTP
+        del request.session['otp']
+        del request.session['email']
+        otp_record.delete()
+
+        return Response({'message': 'User activated successfully'}, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        queryset = UserAccount.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserAccountSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class AccountsUserViewSet(viewsets.ViewSet):
+    permission_classes_by_action = {'create': [AllowAny], 'list': [IsAdminUser], 'verify_otp': [AllowAny], 'default': [IsAuthenticated]}
+
+    def get_permissions(self):
+        return [permission() for permission in self.permission_classes_by_action.get(self.action, self.permission_classes_by_action['default'])]
+
+    def list(self, request):
+        try:
+            users = UserAccount.objects.all()
+            serializer = UserAccountSerializer(users, many=True, context={"request": request})
+            response_data = serializer.data
+            response_dict = {"error": False, "message": "All Users List Data", "data": response_data}
+
+        except ValidationError as e:
+            response_dict = {"error": True, "message": "Validation Error", "details": str(e)}
+        except Exception as e:
+            response_dict = {"error": True, "message": "An Error Occurred", "details": str(e)}
+
+        return Response(response_dict,
+                        status=status.HTTP_400_BAD_REQUEST if response_dict['error'] else status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            # Hash the password before saving the user
+            password = make_password(serializer.validated_data['password'])
+            serializer.validated_data['password'] = password
+
+            # Generate OTP
+            otp = random.randint(100000, 999999)
+            email = serializer.validated_data['email']
+
+            # Send OTP to email
+            send_mail(
+                'Your OTP Code',
+                f'Your OTP code is {otp}',
+                'from@example.com',
+                [email],
+                fail_silently=False,
+            )
+
+            # Save OTP to the database
+            OTP.objects.create(email=email, otp=otp)
+
+            # Set the user as inactive
+            serializer.validated_data['is_active'] = False
+            serializer.save()
+
+            # Save OTP and email in session
+            request.session['otp'] = otp
+            request.session['email'] = email
+
+            return Response({'message': 'OTP sent to email'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def verify_otp(self, request):
+        otp = request.data.get('otp')
+
+        if not otp:
+            return Response({'error': 'Missing OTP'}, status=status.HTTP_400_BAD_REQUEST)
+
+        email = request.session.get('email')
+        session_otp = request.session.get('otp')
+
+        if otp != str(session_otp):
+            return Response({'error': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            otp_record = OTP.objects.get(email=email, otp=otp)
+        except OTP.DoesNotExist:
+            return Response({'error': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not otp_record.is_valid():
+            return Response({'error': 'OTP expired'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Activate the user
+        try:
+            user = UserAccount.objects.get(email=email)
+            user.is_active = True
+            user.user_type = 'accounts'
             user.save()
         except UserAccount.DoesNotExist:
             return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
