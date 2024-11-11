d25ad6c7cc74099d9d47f107110710b5ec018c61
serenade18 <seretaahmed18@gmail.com>
2024-11-11 12:46:16 +0300

    added verify otp endpoints

--- barakaProject/urls.py
+++ barakaProject/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from barakaApp.views import AdminUserViewSet, SalesUserViewSet, HybridUserViewSet
+from barakaApp.views import AdminUserViewSet, SalesUserViewSet, HybridUserViewSet, UserInfoView
 
 router = routers.DefaultRouter()
 router.register(r'admin/users', AdminUserViewSet, basename='admin-user')
@@ -17,4 +17,8 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/gettoken/', TokenObtainPairView.as_view(), name="gettoken"),
     path('api/refresh_token/', TokenRefreshView.as_view(), name="refresh_token"),
+    path('api/admin/verify/', AdminUserViewSet.as_view({'post': 'verify_otp'}), name='admin-verify'),
+    path('api/hybrid/verify/', HybridUserViewSet.as_view({'post': 'verify_otp'}), name='hybrid-verify'),
+    path('api/sales/verify/', SalesUserViewSet.as_view({'post': 'verify_otp'}), name='sales-verify'),
+    path('api/userinfo/', UserInfoView.as_view(), name='userinfo'),
 ]
